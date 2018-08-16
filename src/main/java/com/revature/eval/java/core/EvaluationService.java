2e7abd799fdad095a8899b073f7c310749fcfc98
Chandrika Sinha <sinhac@rpi.edu>
2018-08-15 22:39:25 -0400

    implemented Question 4

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -1,6 +1,8 @@
 package com.revature.eval.java.core;
 
 import java.time.temporal.Temporal;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -133,9 +135,46 @@ public class EvaluationService {
 	 * @param string
 	 * @return
 	 */
+	
 	public int getScrabbleScore(String string) {
-		// TODO Write an implementation for this method declaration
-		return 0;
+		string = string.toUpperCase();
+		HashMap<Character, Integer> letterValues = new HashMap<>();
+		letterValues.put('A', 1);
+		letterValues.put('E', 1);
+		letterValues.put('I', 1);
+		letterValues.put('O', 1);
+		letterValues.put('U', 1);
+		letterValues.put('L', 1);
+		letterValues.put('N', 1);
+		letterValues.put('R', 1);
+		letterValues.put('S', 1);
+		letterValues.put('T', 1);
+		letterValues.put('D', 2);
+		letterValues.put('G', 2);
+		letterValues.put('B', 3);
+		letterValues.put('C', 3);
+		letterValues.put('M', 3);
+		letterValues.put('P', 3);
+		letterValues.put('F', 4);
+		letterValues.put('H', 4);
+		letterValues.put('V', 4);
+		letterValues.put('W', 4);
+		letterValues.put('Y', 4);
+		letterValues.put('K', 5);
+		letterValues.put('J', 8);
+		letterValues.put('X', 8);
+		letterValues.put('Q', 10);
+		letterValues.put('Z', 10);
+		
+		int result = 0;
+		for (char letter : string.toCharArray()) {
+			if (!letterValues.containsKey(letter)) {
+				// throw error
+			} 
+			result+=(letterValues.get(letter));
+		}
+		
+		return result;
 	}
 
 	/**
